730acbaafa9d64e544bc978cc560f4d4cf1fdbca
ustullycarr <ustullycarr@gmail.com>
2019-03-07 12:29:14 -0500

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject ThirdPersonController;
+
+    private Vector3 offset;
+    
+
+        // Start is called before the first frame update
+    void Start()
+    {
+         offset = transform.position - ThirdPersonController.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = ThirdPersonController.transform.position + offset;
+    }
+}
